1043a334b25b53a5b65a603a8cc70a40ac47d12a
qepikclicker <adminci27@proton.me>
2024-11-01 01:34:02 +0400

    Update bot.php

--- qepik/bot.php
+++ qepik/bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "7334727798:AAHB2oDeuwKn_-ZnwhJvL7AFcrI0Ek3P9NI";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
